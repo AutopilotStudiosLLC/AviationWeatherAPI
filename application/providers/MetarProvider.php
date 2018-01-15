ff227e91fa73ac5a1511f24cfa3847c3243c4e60
Scott Henscheid <contact@ironpilot.net>
2018-01-14 22:12:11 -0700

    Fix METARs for multiple sky conditions.

--- application/providers/MetarProvider.php
+++ application/providers/MetarProvider.php
@@ -90,15 +90,18 @@ class MetarProvider extends RestfulController
 			{
 				$sky = $metar->sky_condition;
 				if(count($sky) == 0) continue;
-				$unsetters = [];
-				foreach($sky->attributes() as $key=>$value)
+				foreach($sky as $condition)
 				{
-					$sky->addChild($key, $value);
-					$unsetters[] = $key;
-				}
-				foreach($unsetters as $attribute)
-				{
-					unset($sky[$attribute]);
+					$unsetters = [];
+					foreach($condition->attributes() as $key => $value)
+					{
+						$condition->addChild($key, $value);
+						$unsetters[] = $key;
+					}
+					foreach($unsetters as $attribute)
+					{
+						unset($condition[$attribute]);
+					}
 				}
 			}
 			return Json::success($xml);
